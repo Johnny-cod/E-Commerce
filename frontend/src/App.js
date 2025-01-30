7b0824577a8998cdf1bc1c184487a0bdf45e2f63
Johnny-cod <jonathanpaul696@gmail.com>
2025-01-31 02:33:14 +0530

    Pages Creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
 
 function App() {
   return (
     <div>
+      <BrowserRounter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+
+      </Routes>
+      </BrowserRounter>
     </div>
   );
 }
